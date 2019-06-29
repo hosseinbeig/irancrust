39a0876a734c6d237e0a9a4be431dce8c3b4ad87
hosseinbeig <h.beig@yahoo.com>
2019-06-29 13:39:08 +0800

    routes defined

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+// server
 const express = require('express');
+// database
+const connentDB = require('./config/db');
+
+// connect to DB
+connentDB();
 
 const app = express();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
